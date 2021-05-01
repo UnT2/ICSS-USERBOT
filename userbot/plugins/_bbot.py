93afbe3880fa1114a51c35c4c6c3b8b764874ca6
Tony <83485596+UnT2@users.noreply.github.com>
2021-05-01 08:46:19 +0300

    - Ok

---
+++ userbot/plugins/_bbot.py
@@ -0,0 +1,20 @@
+
+from ..tosh import *
+
+@icssbot.on(
+    icss_cmd(
+    outgoing=True, 
+    pattern="سورس$"
+    )
+)
+@icssbot.on(
+    sudo_cmd(pattern="سورس$", allow_sudo=True)
+)
+async def ica(icss):
+    ics_id = await rd(icss)
+    await icss.client.send_file(
+        icss.chat_id, ICSS_IMG, caption=ics_c, reply_to=ics_id
+    )
+    await icss.delete()
+
+CMD_HELP.update({"alive": "{}".format(alv)})
