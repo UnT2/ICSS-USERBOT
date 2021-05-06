e6699c296598d620a1fa69bf1f177ce185159608
Tony <83485596+UnT2@users.noreply.github.com>
2021-05-06 05:56:54 +0300

    - Ok

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -6,5 +6,14 @@ async def ica(icss):
         icss.chat_id, ICSS_IMG, caption=ics_c
     )
     await icss.delete()
+
+@icssbot.on(icss_cmd(pattern="ping")
+async def ping(e):
+    S = datetime.now()
+    e = await eor(e, "⌭")
+    E = datetime.now()
+    M = (E - S).microseconds / 1000
+    await eor(e, Ping.format(M))
+
          
 CMD_HELP.update({"alive": "{}".format(alv)})
