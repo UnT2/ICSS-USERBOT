be1ceee18378484fec355850b3805188187ff893
Tony <83485596+UnT2@users.noreply.github.com>
2021-05-06 06:40:38 +0300

    - Ok

--- resources/Formation/userbot_utils/_utils.py
+++ resources/Formation/userbot_utils/_utils.py
@@ -740,6 +740,17 @@ def asst_cmd(dec):
 
     return kim
 
+T = Config.COMMAND_HAND_LER
+
+def tosh_cmd(dec):
+    def kim(func):
+        pattern = T + dec
+        bot.add_event_handler(
+            func, events.NewMessage(incoming=True, pattern=pattern)
+        )
+
+    return kim
+
 def owner():
     def decorator(func):
         @functools.wraps(func)
