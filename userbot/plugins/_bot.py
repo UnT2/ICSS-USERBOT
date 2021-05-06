ca9c5ddcc20080c92ff2fa2be0a67b2c99d71c16
Tony <83485596+UnT2@users.noreply.github.com>
2021-05-06 06:21:03 +0300

    - Ok

--- userbot/plugins/_bot.py
+++ userbot/plugins/_bot.py
@@ -2,7 +2,6 @@ from . import *
 
 @icssbot.on(icss_cmd(pattern="السورس"))
 async def ica(icss):
-    _, check_sgnirts = check_data_base_heal_th()
     await icss.client.send_file(
         icss.chat_id, ICSS_IMG, caption=ics_c
     )
