36940076389dc65b0e92d41887ca66969c82912b
Tony <83485596+UnT2@users.noreply.github.com>
2021-05-25 14:56:48 +0300

    -

--- userbot/plugins/__init__.py
+++ userbot/plugins/__init__.py
@@ -1,5 +1,12 @@
 from userbot.tosh import Calc, C # calc text
 # for plugins __init__
 from resources.Formation.plugins import *
+
+from userbot import bot
+
+O = bot.me.id
+Name = bot.me.first_name
+M = "[{}](tg://user?id={})".format(Name, O)
+
 from userbot.tosh import *
 
